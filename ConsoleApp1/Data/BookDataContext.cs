a6f46de0428c3d735f7fd607f47a820b7103de7b
cemgr <cemgr@CEM>
2023-03-26 21:10:52 +0300

    Add project files.

---
+++ ConsoleApp1/Data/BookDataContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    public class BookDataContext : DbContext
+    {
+        public BookDataContext(DbContextOptions<BookDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+
+
+    public void InsertBook(Book book)
+    {
+        Books.Add(book);
+        SaveChanges();
+    }
+
+    public void UpdateBook(Book book)
+    {
+        Books.Update(book);
+        SaveChanges();
+    }
+
+    public Book GetBookById(int id)
+    {
+        return Books.Include(b => b.Publisher).FirstOrDefault(b => b.Id == id);
+    }
+
+    public IEnumerable<Book> GetAllBooks()
+    {
+        return Books.Include(b => b.Publisher).ToList();
+    }
+}
